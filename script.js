4edffee5d5da84ebe6c3158f7b67d99f071fce74
Reece Nunez <reecenunez@hotmail.com>
2023-10-19 04:13:45 -0500

    added nodemailer and finished sections

--- script.js
+++ script.js
@@ -22,3 +22,102 @@ window.addEventListener("scroll", function() {
     navbar.style.backgroundColor = "transparent";
   }
 });
+
+
+// advanced scrolling
+$("a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
+    });
+  }
+});
+
+// title animation
+
+$(document).ready(function() {
+  $("#about-me").hide().fadeIn(3000);
+});
+
+// experience animation
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scrollPos = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var elementPos = $('.experience-container').offset().top;
+
+    // Check if the .experience-container is in the viewport
+    if (scrollPos + windowHeight > elementPos) {
+
+      // Animate tech icons from the left
+      $('.tech-grid-icons').each(function(i) {
+        setTimeout(function() {
+          $('.tech-grid-icons').eq(i).animate({
+            left: '0',
+            opacity: '1'
+          }, 2000);
+        }, 200 * (i + 1));
+      });
+
+      // Animate cert icons from the right
+      $('.cert-grid-icons').each(function(i) {
+        setTimeout(function() {
+          $('.cert-grid-icons').eq(i).animate({
+            right: '0',
+            left: 'auto',
+            opacity: '1'
+          }, 2000);
+        }, 200 * (i + 1));
+      });
+
+    }
+  });
+});
+
+//experience animation
+
+$(document).ready(function() {
+  // Set elements to be hidden initially
+  $(".school-header h1, .school-header h2, .school-header p").addClass("hidden");
+
+  $(window).scroll(function() {
+    var scrollPos = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var elementPos = $('.school-header-container').offset().top;
+
+    // Check if the element is in the viewport
+    if (scrollPos + windowHeight > elementPos) {
+      $('#school-title').addClass('slide-in-from-left');
+      $('.school-header .big-header').addClass('slide-in-from-right');
+      $('.school-header p').addClass('slide-in-from-left');
+    }
+  });
+});
+
+// project animation
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      
+      $(".project-container .project1, .project-container .project2").each(function(){
+          /* Check the location of each desired element */
+          var objectBottom = $(this).offset().top + $(this).outerHeight();
+          
+          /* If the element is completely within bounds of the window, fade it in */
+          if (objectBottom < windowBottom) {
+              if ($(this).css("opacity")==0) {$(this).fadeTo(500,2).css('transform', 'translateX(0)');}
+          } else { //object comes into view (scrolling up)
+              if ($(this).css("opacity")==1) {$(this).fadeTo(500,0).css('transform', 'translateX(100px)');}
+          }
+      });
+  }).scroll(); //invoke scroll-handler on page-load
+});
+
+
+
