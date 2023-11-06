56019d8eb4cbdb75b941bee39d5112e66ce5fbfb
Reece <reecenunez@hotmail.com>
2023-11-05 21:56:44 -0600

    added top arrow

--- script.js
+++ script.js
@@ -112,5 +112,17 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
 });
 
 
+// JavaScript code to scroll smoothly
+document.getElementById('up-arrow').addEventListener('click', function(event) {
+  event.preventDefault();
+
+  const targetId = this.getAttribute('href').substring(1);
+  const targetElement = document.getElementById(targetId);
+
+  window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
+  });
+});
 
 
